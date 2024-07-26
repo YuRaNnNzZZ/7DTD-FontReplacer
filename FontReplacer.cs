a1128e1b6ed8ce6de0e792b2f7354d7edacff0bd
YuRaNnNzZZ <medved609@yandex.ru>
2024-07-27 00:08:40 +0300

    code deduplication + font preload

--- FontReplacer.cs
+++ FontReplacer.cs
@@ -9,13 +9,8 @@ public class FontReplacer : IModApi
 {
     public static string modFullPath;
 
-    public static bool loaded = false;
-    public static string fontBundlePath;
-    public static string fontName;
-
-    public static bool loaded2 = false;
-    public static string consoleFontBundlePath;
-    public static string consoleFontName;
+    public static Font mainFont = null;
+    public static Font consoleFont = null;
 
     public void InitMod(Mod mod)
     {
@@ -46,57 +41,56 @@ public class FontReplacer : IModApi
             {
                 if (xnode.Name == "Font")
                 {
-                    XmlNode bundlePathNode = xnode.Attributes.GetNamedItem("BundlePath");
-                    fontBundlePath = bundlePathNode?.Value?.ToString();
+                    if (mainFont == null)
+                    {
+                        mainFont = TryParseAndLoadFont(xnode);
+                    }
 
-                    XmlNode nameNode = xnode.Attributes.GetNamedItem("Name");
-                    fontName = nameNode?.Value?.ToString();
+                    continue;
                 }
 
                 if (xnode.Name == "ConsoleFont")
                 {
-                    XmlNode bundlePathNode = xnode.Attributes.GetNamedItem("BundlePath");
-                    consoleFontBundlePath = bundlePathNode?.Value?.ToString();
+                    if (consoleFont == null)
+                    {
+                        consoleFont = TryParseAndLoadFont(xnode);
+                    }
 
-                    XmlNode nameNode = xnode.Attributes.GetNamedItem("Name");
-                    consoleFontName = nameNode?.Value?.ToString();
+                    continue;
                 }
             }
         }
+    }
 
-        loaded = fontBundlePath != null && fontName != null;
-        loaded2 = consoleFontBundlePath != null && consoleFontName != null;
+    private Font TryParseAndLoadFont(XmlNode xnode)
+    {
+        XmlNode bundlePathNode = xnode.Attributes.GetNamedItem("BundlePath");
+        string fontBundlePath = bundlePathNode?.Value?.ToString();
+
+        XmlNode nameNode = xnode.Attributes.GetNamedItem("Name");
+        string fontName = nameNode?.Value?.ToString();
+
+        string fullBundlePath = modFullPath + "/" + fontBundlePath;
+
+        if (!File.Exists(fullBundlePath))
+        {
+            Log.Error("[Font Replacement][" + xnode.Name + "] File '" + fontBundlePath + "' not found in mod folder!");
+
+            return null;
+        }
+
+        Log.Out("[Font Replacement][" + xnode.Name + "] Loading replacement font '" + fontName + "' from '" + fullBundlePath + "'");
+        return DataLoader.LoadAsset<Font>("#" + fullBundlePath + "?" + fontName);
     }
 
     [HarmonyPatch(typeof(XUi), nameof(XUi.GetUIFontByName))]
     public class ReplaceReferenceFont
     {
-        private static Font newFont;
-
-        public static NGUIFont Postfix(NGUIFont __result, string _name, bool _showWarning)
+        public static NGUIFont Postfix(NGUIFont __result, string _name)
         {
-            if (loaded && _name == "ReferenceFont" && __result != null)
+            if (mainFont != null && __result != null && _name == "ReferenceFont" && __result.dynamicFont != mainFont)
             {
-                if (newFont == null)
-                {
-                    string fullBundlePath = modFullPath + "/" + fontBundlePath;
-
-                    if (!File.Exists(fullBundlePath))
-                    {
-                        Log.Error("[Font Replacement] File '" + fontBundlePath + "' not found in mod folder!");
-
-                        loaded = false;
-                        return __result;
-                    }
-
-                    Log.Out("[Font Replacement] Loading replacement font '" + fontName + "' from '" + fullBundlePath + "'");
-                    newFont = DataLoader.LoadAsset<Font>("#" + fullBundlePath + "?" + fontName);
-                }
-
-                if (__result.dynamicFont != newFont)
-                {
-                    __result.dynamicFont = newFont;
-                }
+                __result.dynamicFont = mainFont;
             }
 
             return __result;
@@ -106,30 +100,11 @@ public class FontReplacer : IModApi
     [HarmonyPatch(typeof(GUIWindowConsole), "AllocText")]
     public class ReplaceConsoleFont
     {
-        private static Font newFont;
-
         public static Text Postfix(Text __result)
         {
-            if (!loaded2) return __result;
-
-            if (newFont == null)
+            if (consoleFont != null && __result != null && __result.font != consoleFont)
             {
-                string fullBundlePath = modFullPath + "/" + consoleFontBundlePath;
-
-                if (!File.Exists(fullBundlePath))
-                {
-                    Log.Error("[Font Replacement] File '" + consoleFontBundlePath + "' not found in mod folder!");
-                    loaded2 = false;
-                    return __result;
-                }
-
-                Log.Out("[Font Replacement] Loading console replacement font '" + consoleFontName + "' from '" + fullBundlePath + "'");
-                newFont = DataLoader.LoadAsset<Font>("#" + fullBundlePath + "?" + consoleFontName);
-            }
-
-            if (__result != null && __result.font != newFont)
-            {
-                __result.font = newFont;
+                __result.font = consoleFont;
             }
 
             return __result;
